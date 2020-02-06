7c730df9fbc79cd9a47b1ddcdedc69d8d637b714
ittyBittyBytes <loralie.flint0128@gmail.com>
2020-02-05 17:51:54 -0700

    tests done for user router

--- users/user-router.spec.js
+++ users/user-router.spec.js
@@ -1,31 +1,39 @@
 const db = require("../data/db-config");
-const request = require('supertest')
+const request = require("supertest");
 const users = require("./user-router");
-const server = require("../server")
-describe("user router", () => {
-    beforeEach(async () => {
-      await db("users").truncate();
-    });
+const server = require("../server");
 
-    describe("/login", () => {
-        test("should return a 200 status", () => {
-          return request(server)
-            .post("/login")
-            .send({ username: "legend10", password: "paws" })
-            .then(res => {
-              expect(res.status).toBe(200);
-            });
-        });
+// register a user
+describe("/register", () => {
+  test("should return a 201 status", () => {
+    return request(server)
+      .post("/register")
+      .send({ username: "a", password: "pass" })
+      .then(res => {
+        expect(res.status).toBe(201);
       });
+  });
+});
 
-    // describe("/register", () => {
-    //     test("should return a 200 status", () => {
-    //       return request(users)
-    //         .post("/register")
-    //         .then(res => {
-    //           expect(res.status).toBe(200);
-    //         });
-    //     });
-    //   });
+// login a user
+describe("/login", () => {
+  test("should return a 200 status", () => {
+    return request(server)
+      .post("/login")
+      .send({ username: "Rachel", password: "password" })
+      .then(res => {
+        expect(res.status).toBe(200);
+      });
+  });
+});
 
-})
+// delete a user by id
+describe("/delete", () => {
+  test("should return a 200 status", () => {
+    return request(server)
+      .delete("/delete/4")
+      .then(res => {
+        expect(res.status).toBe(200);
+      });
+  });
+});
